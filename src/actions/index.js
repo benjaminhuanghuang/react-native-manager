8a9eda0ebc715aaf3cccb5270a4763ea1efa1a80
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-10 11:35:43 -0700

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,7 +9,7 @@
 //
 import firebase from 'firebase';
 
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL } from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -25,11 +25,23 @@ export const passwordChanged = (text) => {
   }
 }
 
+// return a function
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user })
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      })
+      .catch(()=>{
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+        .then( user=>{
+          dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+        })
+        .catch(()=> loginUserFail(dispatch));
       });
   };
+}
+
+const loginUserFail = (dispatch) =>{
+  dispatch({type: LOGIN_USER_FAIL});
 }
